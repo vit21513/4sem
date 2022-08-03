8e4a3bea9bc8eacbfcd841a1c23387bc8059b682
vit21513 <vit21513@yandex.ru>
2022-08-03 14:48:11 +0300

    add task27

---
+++ task27/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int Digits (int numbers)
+{
+   int index = 1, digit = 0;
+   while (index <= num)        //определяем  из скольки чисел состоит число  и передаем в переменную а
+{
+    digit = digit + 1;
+    index = index * 10;
+}
+return  digit;}
+
+int dig= Digits(num);
+
+
+
+int[] nums = new int[dig];         // обьявлем массив размером в количество символов введеннного числа
+//int indexArray = nums.Length - 1;
+//while (indexArray >= 0)
+//{
+//    nums[indexArray] = num % 10;
+//    num = num / 10;
+ //   indexArray = indexArray - 1;
+
+//}
+
+int SumNumber (int digg)
+{
+    int sumnumb = 0;
+for (int i = 0; i < digg; i++)
+{
+    sumnumb = sumnumb + nums[i];
+}return sumnumb;
+}
+int sum = SumNumber(dig);
+
+Console.WriteLine($" сумму цифр в числе составляет  {sum} ");
+
