23973be77ed963557d1e8b103be3031518d5c4f3
vit21513 <vit21513@yandex.ru>
2022-08-03 15:02:40 +0300

    temp

--- task27/Program.cs
+++ task27/Program.cs
@@ -7,38 +7,43 @@ Console.WriteLine("введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 
-int Digits (int numbers)
+int Digits(int numbers)    //определяем  из скольки чисел состоит число  и передаем в переменную digit
 {
-   int index = 1, digit = 0;
-   while (index <= num)        //определяем  из скольки чисел состоит число  и передаем в переменную а
-{
-    digit = digit + 1;
-    index = index * 10;
+    int digit = 0;
+    for (int i = 1; i <= num; i = i * 10)
+    {
+        digit = digit + 1;
+    }
+    return digit;
 }
-return  digit;}
-
-int dig= Digits(num);
 
 
+int dig = Digits(num);   // переменная хранит число регистров
 
 int[] nums = new int[dig];         // обьявлем массив размером в количество символов введеннного числа
-//int indexArray = nums.Length - 1;
-//while (indexArray >= 0)
-//{
-//    nums[indexArray] = num % 10;
-//    num = num / 10;
- //   indexArray = indexArray - 1;
 
-//}
 
-int SumNumber (int digg)
+int indexArray = nums.Length - 1;
+while (indexArray >= 0)
+{
+    nums[indexArray] = num % 10;
+    num = num / 10;
+    indexArray = indexArray - 1;
+
+}
+
+
+int SumNumber(int digg)
 {
     int sumnumb = 0;
-for (int i = 0; i < digg; i++)
-{
-    sumnumb = sumnumb + nums[i];
-}return sumnumb;
+    for (int i = 0; i < digg; i++)
+    {
+        sumnumb = sumnumb + nums[i];
+    }
+    return sumnumb;
 }
+
+
 int sum = SumNumber(dig);
 
 Console.WriteLine($" сумму цифр в числе составляет  {sum} ");
